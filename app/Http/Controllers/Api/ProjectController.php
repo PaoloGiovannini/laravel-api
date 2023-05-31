784d980f51d00cd556248c0c9104a3234e244ce2
Paolo Giovannini <giovannini13@gmail.com>
2023-05-31 16:26:16 +0200

    rotta per singola visualizzazione card

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -8,13 +8,22 @@ use Illuminate\Http\Request;
 
 class ProjectController extends Controller
 {
-        public function index() {
+    public function index(){
         $projects = Project::with(['type', 'technologies'])->paginate(3);
-
         return response()->json([
             'success' => true,
             'results' => $projects
         ]);
+    }
+    public function show($slug) {
+
+        $project = Project::where('slug', $slug)->with(['type', 'technologies'])->first();
+
+        return response()->json([
+            'success' => true,
+            'project' => $project
+        ]);
+
 
     }
 }
